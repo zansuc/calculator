1097341a9aaee02eadf7da24356e9cbd6c23cfa3
zansuc <zan@students.amikom.ac.id>
2025-03-13 11:33:01 +0700

    update

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,22 @@ class calculator
 {
 	static word void main ( string [] args )
 	{
+		int a = 10;
+		int b = 6;
 		
+		Consule.WriteLine ( " Hasil penambahan : {0} + {1} = {2}", a, b, Penambahan (a, b ));
+		Consule.WriteLine ( " Hasil pengurangan : {0} - {1} = {2}", a, b, Pengurangan (a, b ));
+		
+		Console.WriteLine( " \n Tekan sembarang key untuk keluar ");
+		Consule.ReadKey();
+		
+	}
+	static int Penambahan (int a, int b)
+	{
+		return a + b;
+	}
+	static int Pengurangan (int a, int b)
+	{
+		return a - b;
 	}
 }
